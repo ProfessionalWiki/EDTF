e42cab6fd73dc95bc1c65231e113c50cebedab71
Jeroen De Dauw <jeroendedauw@gmail.com>
2021-02-09 00:05:02 +0100

    Refactor tests to match prod code

---
+++ tests/Unit/Humanize/Languages/EnglishHumanizerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace EDTF\Tests\Unit\Humanize\Languages;
+
+use EDTF\Humanize\Languages\EnglishHumanizer;
+use EDTF\Season;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \EDTF\Humanize\Languages\EnglishHumanizer
+ */
+class EnglishHumanizerTest extends TestCase {
+
+	/**
+	 * @dataProvider seasonProvider
+	 */
+	public function testSeasons( string $expected, Season $season ): void {
+		$this->assertSame(
+			$expected,
+			( new EnglishHumanizer() )->humanize( $season )
+		);
+	}
+
+	public function seasonProvider(): \Generator {
+		yield [ 'Spring 2001', new Season( 2001, 21 ) ];
+		yield [ 'Summer 1234', new Season( 1234, 22 ) ];
+		yield [ 'Autumn 10000', new Season( 10000, 23 ) ];
+		yield [ 'Winter 42', new Season( 42, 24 ) ];
+		yield [ 'Winter 0', new Season( 0, 24 ) ];
+		yield [ 'Winter -1', new Season( -1, 24 ) ];
+		yield [ 'Quarter 1 2001', new Season( 2001, 33 ) ];
+		yield [ 'Quadrimester 2 2001', new Season( 2001, 38 ) ];
+		yield [ 'Semester 2 2001', new Season( 2001, 41 ) ];
+		yield [ 'Autumn (Southern Hemisphere) 2001', new Season( 2001, 31 ) ];
+	}
+
+}
