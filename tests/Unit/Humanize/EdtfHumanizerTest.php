5607fd75804a6599b94a0524413a6865bfa09b21
Jeroen De Dauw <jeroendedauw@gmail.com>
2021-02-08 23:26:46 +0100

    Test the more fancy seasons

--- tests/Unit/Humanize/EdtfHumanizerTest.php
+++ tests/Unit/Humanize/EdtfHumanizerTest.php
@@ -28,6 +28,12 @@ class EdtfHumanizerTest extends TestCase {
 		yield [ 'Summer 1234', new Season( 1234, 22 ) ];
 		yield [ 'Autumn 10000', new Season( 10000, 23 ) ];
 		yield [ 'Winter 42', new Season( 42, 24 ) ];
+		yield [ 'Winter 0', new Season( 0, 24 ) ];
+		yield [ 'Winter -1', new Season( -1, 24 ) ];
+		yield [ 'Quarter 1 2001', new Season( 2001, 33 ) ];
+		yield [ 'Quadrimester 2 2001', new Season( 2001, 38 ) ];
+		yield [ 'Semester 2 2001', new Season( 2001, 41 ) ];
+		yield [ 'Autumn (Southern Hemisphere) 2001', new Season( 2001, 31 ) ];
 	}
 
 }
