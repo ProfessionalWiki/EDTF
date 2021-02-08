892c4766c32f87f6c4826a1d0db5fd39ead48bbd
Jeroen De Dauw <jeroendedauw@gmail.com>
2021-02-08 16:56:00 +0100

    Add some TODOs

--- src/Utils/DatetimeFactory/DatetimeFactoryInterface.php
+++ src/Utils/DatetimeFactory/DatetimeFactoryInterface.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace EDTF\Utils\DatetimeFactory;
 
+/**
+ * FIXME: return type missing
+ * TODO: what value does this interface bring?
+ * FIXME: Utils is a bad NS. Also: maybe this should be PackagePrivate
+ */
 interface DatetimeFactoryInterface
 {
     /**
