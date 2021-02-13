c8c354a5c28863dbe74460a535294578ffc05648
Chaudry <chaudbak@gmail.com>
2021-02-13 11:09:08 +0200

    Refactor Parser. Introduce new classes to encapsulate data

---
+++ src/PackagePrivate/ValueObjects/Composites/Timezone.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace EDTF\PackagePrivate\ValueObjects\Composites;
+
+class Timezone
+{
+    private ?int $tzMinute;
+    private ?int $tzHour;
+
+    private ?string $tzUtc;
+    private ?string $tzSign;
+
+    public function __construct(?int $tzHour, ?int $tzMinute, ?string $tzSign, ?string $tzUtc)
+    {
+        $this->tzHour = $tzHour;
+        $this->tzMinute = $tzMinute;
+        $this->tzSign = $tzSign;
+        $this->tzUtc = $tzUtc;
+    }
+
+    public function getTzSign(): ?string
+    {
+        return $this->tzSign;
+    }
+
+    public function getTzMinute(): ?int
+    {
+        return $this->tzMinute;
+    }
+
+    public function getTzHour(): ?int
+    {
+        return $this->tzHour;
+    }
+
+    public function getTzUtc(): ?string
+    {
+        return $this->tzUtc;
+    }
+}
