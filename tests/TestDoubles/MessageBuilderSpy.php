a5e33117176ae8a1e254f96d80db31adcd7451a4
Chaudry <chaudbak@gmail.com>
2021-02-24 23:52:03 +0200

    Remove unused class property

--- tests/TestDoubles/MessageBuilderSpy.php
+++ tests/TestDoubles/MessageBuilderSpy.php
@@ -5,18 +5,10 @@ declare( strict_types = 1 );
 namespace EDTF\Tests\TestDoubles;
 
 use EDTF\PackagePrivate\Humanizer\Internationalization\MessageBuilder;
-use PHPUnit\Framework\TestCase;
 
 class MessageBuilderSpy implements MessageBuilder
 {
-    private TestCase $testCase;
-
     private array $buildMessageCalls = [];
-
-    public function __construct(TestCase $testCase)
-    {
-        $this->testCase = $testCase;
-    }
 
     public function buildMessage(string $messageKey, string ...$arguments): string
     {
