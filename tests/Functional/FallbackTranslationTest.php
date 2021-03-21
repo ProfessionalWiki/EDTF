266a0dfb7133a7c7cfdda35c867168abd8366863
Chaudry <chaudbak@gmail.com>
2021-03-21 11:52:45 +0200

    Integration test to check fallback for missing .json file

---
+++ tests/Functional/FallbackTranslationTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace EDTF\Tests\Functional;
+
+use EDTF\EdtfFactory;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \EDTF\PackagePrivate\Humanizer\InternationalizedHumanizer
+ */
+class FallbackTranslationTest extends TestCase
+{
+    private const TEST_RESOURCES_PATH = __DIR__ . '/resources/i18n';
+
+    public function testFallbackEnglishTranslationForJanuary(): void
+    {
+        $humanizer = EdtfFactory::newHumanizerForLanguage('fr', 'en', self::TEST_RESOURCES_PATH);
+
+        $this->assertSame(
+            "January 1980",
+            $humanizer->humanize(EdtfFactory::newParser()->parse('1980-01')->getEdtfValue())
+        );
+
+        $this->assertSame(
+            "Février 1980",
+            $humanizer->humanize(EdtfFactory::newParser()->parse('1980-02')->getEdtfValue())
+        );
+    }
+
+    public function testFallbackEnglishTranslationForMissingJson(): void
+    {
+        $humanizer = EdtfFactory::newHumanizerForLanguage('de', 'en', self::TEST_RESOURCES_PATH);
+
+        $this->assertSame(
+            "January 1980",
+            $humanizer->humanize(EdtfFactory::newParser()->parse('1980-01')->getEdtfValue())
+        );
+    }
+}
