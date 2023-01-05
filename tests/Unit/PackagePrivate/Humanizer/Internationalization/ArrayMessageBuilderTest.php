6317d19b1cd90d50a8c07b166605387acfa49475
thomas-topway-it <topway.it@mail.com>
2023-01-06 00:59:02 +0400

    diversified tests

--- tests/Unit/PackagePrivate/Humanizer/Internationalization/ArrayMessageBuilderTest.php
+++ tests/Unit/PackagePrivate/Humanizer/Internationalization/ArrayMessageBuilderTest.php
@@ -38,10 +38,7 @@ class ArrayMessageBuilderTest extends TestCase {
 	public function testParameters(): void {
 		$builder = new ArrayMessageBuilder(
 			[
-				'right-key' => 'This was written by $2 on $1 ($1)',
-				"edtf-day-and-year" => "$1e{{PLURAL:$1|r|}} jour d’un mois inconnu de $2",
-				"month-number" => "... and $1 month{{PLURAL:$1||s}}",
-				"month-number-missing-argument" => "$1 and $2 month{{PLURAL:$2||s}}",
+				'right-key' => 'This was written by $2 on $1 ($1)'
 			]
 		);
 
@@ -50,30 +47,71 @@ class ArrayMessageBuilderTest extends TestCase {
 			$builder->buildMessage( 'right-key', '2021-02-21', 'Jeroen De Dauw' )
 		);
 
+	}
+
+	public function testPluralArgumentIsSingular(): void {
+		$builder = new ArrayMessageBuilder(
+			[
+				"edtf-day-and-year" => "$1e{{PLURAL:$1|r|}} jour d’un mois inconnu de $2"
+			]
+		);
+
 		$this->assertSame(
 			'1er jour d’un mois inconnu de 1985',
 			$builder->buildMessage( 'edtf-day-and-year', '1', '1985' )
+		);
+	}
+
+	public function testPluralArgumentIsPlural(): void {
+		$builder = new ArrayMessageBuilder(
+			[
+				"edtf-day-and-year" => "$1e{{PLURAL:$1|r|}} jour d’un mois inconnu de $2"
+			]
 		);
 
 		$this->assertSame(
 			'3e jour d’un mois inconnu de 1985',
 			$builder->buildMessage( 'edtf-day-and-year', '3', '1985' )
 		);
+	}
 
-		$this->assertSame(
-			'... and 1 month',
-			$builder->buildMessage( 'month-number', '1' )
+	public function testPluralMultipleArguments(): void {
+		$builder = new ArrayMessageBuilder(
+			[
+				"multiple-arguments" => "day{{PLURAL:$1||s}} $1 and $2 month{{PLURAL:$2||s}}",
+			]
 		);
 
 		$this->assertSame(
-			'... and 3 months',
-			$builder->buildMessage( 'month-number', '3' )
+			'day 1 and 3 months',
+			$builder->buildMessage( 'multiple-arguments', '1', '3' )
+		);
+	}
+
+	public function testPluralMultipleArgumentsInverted(): void {
+		$builder = new ArrayMessageBuilder(
+			[
+				"multiple-arguments-inverted" => "day{{PLURAL:$1||s}} $1 and $2 month{{PLURAL:$2||s}}",
+			]
 		);
 
 		$this->assertSame(
-			'3 and $2 month',
-			$builder->buildMessage( 'month-number-missing-argument', '3' )
-		);	
+			'days 3 and 1 month',
+			$builder->buildMessage( 'multiple-arguments-inverted', '3', '1' )
+		);
+	}
+
+	public function testPluralMultipleArgumentsMissingParameter(): void {
+		$builder = new ArrayMessageBuilder(
+			[
+				"multiple-arguments-missing-parameter" => "day{{PLURAL:$1||s}} $1 and $2 month{{PLURAL:$2||s}}",
+			]
+		);
+
+		$this->assertSame(
+			'days 3 and $2 month',
+			$builder->buildMessage( 'multiple-arguments-missing-parameter', '3' )
+		);
 
 	}
 
